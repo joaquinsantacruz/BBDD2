09fecd9f302d0c0a7a8a10121013a7fcae8d6abe
Juani <juanignaciovergara14@gmail.com>
2025-03-28 11:08:57 -0300

    Add save methods to repository

--- src/main/java/unlp/info/bd2/repositories/ToursRepository.java
+++ src/main/java/unlp/info/bd2/repositories/ToursRepository.java
@@ -1,6 +1,10 @@
 package unlp.info.bd2.repositories;
 
+import unlp.info.bd2.model.*;
 
 public interface ToursRepository {
 
+    void saveRoute(Route route);
+    void saveItem(ItemService item);
+    void savePurchase(Purchase purchase);
 }
--- src/main/java/unlp/info/bd2/repositories/ToursRepositoryImpl.java
+++ src/main/java/unlp/info/bd2/repositories/ToursRepositoryImpl.java
@@ -1,7 +1,57 @@
 package unlp.info.bd2.repositories;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+
+import unlp.info.bd2.model.*;
 
 public class ToursRepositoryImpl implements ToursRepository{
 
-
+    
+    
+        SessionFactory factory;
+    
+        public void saveItem(ItemService item){
+            Transaction tx = null;
+            try(Session session = factory.openSession()){
+                tx = session.beginTransaction();
+                session.persist(item);
+                tx.commit();
+            }
+            catch (Exception e){
+                if(tx != null){
+                    tx.rollback();
+                }
+            }
+        }
+    
+        public void savePurchase(Purchase purchase){
+            Transaction tx = null;
+            try(Session session = factory.openSession()){
+                tx = session.beginTransaction();
+                session.persist(purchase);
+                tx.commit();
+            }
+            catch (Exception e){
+                if(tx != null){
+                    tx.rollback();
+                }
+            }
+        }
+    
+        public void saveRoute(Route route){
+            Transaction tx = null;
+            try(Session session = factory.openSession()){
+                tx = session.beginTransaction();
+                session.persist(route);
+                tx.commit();
+            }
+            catch (Exception e){
+                if(tx != null){
+                    tx.rollback();
+                }
+            }
+        }
+    
 }
