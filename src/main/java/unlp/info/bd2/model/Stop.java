bc4534f1952c2d61c53a24328e65473801d198ec
Juani <juanignaciovergara14@gmail.com>
2025-04-04 22:04:29 -0300

    Change OneToMany to ManyToMany relationship

--- src/main/java/unlp/info/bd2/model/Stop.java
+++ src/main/java/unlp/info/bd2/model/Stop.java
@@ -1,10 +1,13 @@
 package unlp.info.bd2.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,7 +24,16 @@ public class Stop {
     @Column(nullable = false)
     private String description;
 
+    @ManyToMany(mappedBy = "stops")
+    private List<Route> routes;
 
+    public Stop() {
+    }
+
+    public Stop(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
     public Long getId() {
         return id;
     }
