910e52b2cae91fe4e77bd325ee590a9c7852c405
Joaquin Emanuel <79953847+joaquinsantacruz@users.noreply.github.com>
2025-05-07 19:53:50 -0300

    Update UserRepository.java

--- src/main/java/unlp/info/bd2/repositories/UserRepository.java
+++ src/main/java/unlp/info/bd2/repositories/UserRepository.java
@@ -4,6 +4,7 @@ package unlp.info.bd2.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
