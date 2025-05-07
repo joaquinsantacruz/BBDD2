98b115c1686ba4333676270e5b66b3172c4040f1
Joaquin Emanuel <79953847+joaquinsantacruz@users.noreply.github.com>
2025-05-07 19:58:30 -0300

    Update DriverUserRepository.java

--- src/main/java/unlp/info/bd2/repositories/DriverUserRepository.java
+++ src/main/java/unlp/info/bd2/repositories/DriverUserRepository.java
@@ -4,6 +4,7 @@ package unlp.info.bd2.repositories;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import unlp.info.bd2.model.DriverUser;
@@ -11,8 +12,8 @@ import unlp.info.bd2.model.DriverUser;
 @Repository
 public interface DriverUserRepository extends CrudRepository<DriverUser, Long> {
  
-    Optional<DriverUser> getDriverUserByUsername(String username);
+    Optional<DriverUser> findByUsername(String username);
 
     @Query("SELECT d FROM DriverUser d WHERE d.routes.size > 0 ORDER BY d.routes.size DESC")
-    DriverUser getTopDriverUserWithMoreRoutes();
+    DriverUser getTopDriverUserWithMoreRoutes(Pageable pageable);
 }
